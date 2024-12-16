7c1dd3adde13aa827cf3b14ea010e9f9c46019d5
Syedaashnaghazanfar <160127890+Syedaashnaghazanfar@users.noreply.github.com>
2024-12-16 20:47:15 +0500

    Update navbar.tsx

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
           <li><Link href="/">Home</Link></li>
           <li><Link href="/shop">Shop</Link></li>
           <li><Link href="/about">About</Link></li>
-          <li><Link href="/blog">Blog</Link></li>
+          <li><Link href="#">Blog</Link></li>
           <li><Link href="/contact">Contact</Link></li>
           <li><Link href="/pages">Pages</Link></li>
         </ul>
